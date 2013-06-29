d19a54611d32bd51593c6aaa1e6ee55d448d3fbe
Kimbro Staken <kstaken@kstaken.com>
2013-06-28 23:13:49 -0700

    Adding setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(name='dockermix',
+    version='0.1',
+    description='Tools to provision multiple Docker containers via a single command.',
+    author='Kimbro Staken',
+    author_email='kstaken@kstaken.com',
+    url='https://github.com/kstaken/dockermix',
+    packages=['dockermix'],
+    scripts=['scripts/boot.py', 'scripts/shutdown.py']
+)
