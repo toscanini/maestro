87d2c3918213da916129a38db90c9d9e11ae21a2
Kimbro Staken <kstaken@kstaken.com>
2013-06-30 23:27:48 -0700

    Removing the old simple scripts

--- bin/boot.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import sys
-sys.path.append('.')
-from dockermix import dockermix
-
-containers = dockermix.ContainerMix('../dockermix.yml')
-containers.build()
-containers.save()
--- bin/shutdown.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-import sys
-#sys.path.append('../salt-test-runner')
-from dockermix import dockermix
-
-containers = dockermix.ContainerMix(environment='environment.yml')
-containers.destroy()
